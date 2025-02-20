d8f710e665a7a5c981adb277a9aedbe6480b1ef5
Absm Shakib <absmshakib86@gmail.com>
2025-02-21 00:44:50 +0600

    Add GSC meta tag

---
+++ public/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="topv8cJMu8GUh78xfN6MEV6c3KpPn5uvdQEVw80zR7Q" />
+        <title>LS Electricité</title> {/* আপনার বিদ্যমান টাইটেল */}
+      </Head>
+      <div>
+        <h1>Welcome to LS Electricité</h1> {/* আপনার বিদ্যমান কনটেন্ট */}
+      </div>
+    </>
+  );
+}
